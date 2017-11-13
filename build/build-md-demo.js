db4696fce224011a585616cded2b948c0b74558e
zhuhui <huizhu@wisedu.com>
2017-11-13 16:14:57 +0800

    添加reset

--- build/build-md-demo.js
+++ build/build-md-demo.js
@@ -1,12 +1,24 @@
 const fs = require('fs')
 const path = require('path')
+const args = process.argv.splice(2);
 
 const src = 'src/markdown/mobile'
 fs.readdirSync(src).forEach(md => {
   const filePath = src + '/' + md
   let content = fs.readFileSync(filePath, 'utf-8')
-  if (!/::: demo/.test(content)) {
-    content = content.replace(/```html[^`]+```/g, val => '::: demo\n' + val + '\n:::')
-    fs.writeFileSync(filePath, content)
+
+  if (args[0] === 'reset') {
+    content = content.replace(/::: demo\n/g, '').replace(/\n:::/g, '').replace(/<\!-- demo start -->[^@]+<\!-- demo end -->/g, '')
+  } else {
+    if (!/::: demo/.test(content)) {
+      content = content.replace(/```html[^`]+```/g, val => '::: demo\n' + val + '\n:::')
+      // content = content.replace(/```javascript[^`]+```/g, val => {
+      //   if (val.indexOf('import') > -1) return ''
+      //   const text = val.replace(/^```javascript\n|```$/g, '')
+      //   return val + '\n<!-- demo start -->\n<script>\n' + text + '</script>\n<!-- demo end -->\n'
+      // })
+    }
   }
+  fs.writeFileSync(filePath, content)
+
 })
