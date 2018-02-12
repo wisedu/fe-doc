6d9d96abcc47b3d283a956d03bcb6c37f7a3b1c9
戚雨 <yuqi@wisedu.com>
2018-02-12 17:18:38 +0800

    组合的评论联动功能

--- src/components/side-nav.vue
+++ src/components/side-nav.vue
@@ -76,12 +76,12 @@ export default {
     getCookie(c_name){
       if (document.cookie.length>0){
         let c_start=document.cookie.indexOf(c_name + "=")
-        if (c_start!=-1){ 
-          c_start=c_start + c_name.length+1 
+        if (c_start!=-1){
+          c_start=c_start + c_name.length+1
           let c_end=document.cookie.indexOf(";",c_start)
           if (c_end==-1) c_end=document.cookie.length
           return unescape(document.cookie.substring(c_start,c_end))
-        } 
+        }
       }
       return ""
     },
@@ -107,6 +107,7 @@ export default {
           categoryId: id
         }
       })
+      this.$emit("navchange", id);
     },
     getCategories(){
       let that = this;
